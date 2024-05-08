62f70a4fbce6dce799ec5ac6c728f35fef2f6587
Andrey42r <168967968+Andrey42r@users.noreply.github.com>
2024-05-08 20:09:11 +0700

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+#immutable_var = tuple = (1, 2, 3, 4, "car", "scooter", "bike")
+#print(immutable_var)
+#immutable_var [0] = 4 #Объект tuple нельзя изменить после создания
+
+mutable_list = ["car", "scooter", "bike"]
+mutable_list[0] = "loader"
+print(mutable_list)

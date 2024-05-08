f4e576e22d6c226a4202a53d587db6c53cef9cd1
Andrey42r <168967968+Andrey42r@users.noreply.github.com>
2024-05-08 18:53:13 +0700

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,20 @@
+#name = input("Как ваше имя? ")
+#print("Здравствуйте,", name)
+#current_year = 2024
+#your_age = int(input("Сколько вам лет? "))
+#year_of_birth = current_year - your_age
+#print ("Значит вы родились в", year_of_birth, "году")
+#if your_age <= 16:
+#    print("Вероятно, вы учитесь в школе)")
+#if your_age >20:
+#    print("Скорее всего вы студент, или работаете)")
+#question = int(input("Я угадал? 1 - Да; 2 - Нет "))
+#if question <= 1:
+#    print("Я очень рад :)")
+#if question >= 2:
+#    print("Очень жаль :(")
+my_string = input()
+print(len(my_string))
+print(my_string.upper().replace(" ",""))
+print(my_string[0])
+print(my_string[-1])

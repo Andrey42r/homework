f02564f0879eecf20fc83604931e58bbe53bc47a
Andrey42r <168967968+Andrey42r@users.noreply.github.com>
2024-05-09 20:36:24 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,16 @@
+my_list = ["blue", "orange", "black", "green", "yellow"]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[3:6])
+my_list[2] = "gray"
+print(my_list)
+
+
+my_dict = {"blue": "синий", "orange": "оранжевый", "black": "черный", "green": "зеленый", "yellow": "желтый"}
+print(my_dict)
+print(my_dict["blue"])
+my_dict["blue"] = "голубой"
+print(my_dict)
+my_dict.update({"red": "красный"})
+print(my_dict)

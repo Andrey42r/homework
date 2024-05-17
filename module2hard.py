d6e773f38d489e29aa0419595dc636c14e1ae270
Andrey42r <168967968+Andrey42r@users.noreply.github.com>
2024-05-17 14:58:11 +0700

    Add files via upload

--- module2hard.py
+++ module2hard.py
@@ -1,8 +1,8 @@
 n = int(input("Введите число от 3 до 20: "))
-result = ""
+result = "Пароль для заданного числа: "
 
-for i in range(1, n - 1):
-    for j in range(i + 1, n):
-        if i != j and i + j == n:
+for i in range(1, n-1):
+    for j in range(i+1, n):
+        if n % (i + j) == 0:
             result += str(i) + str(j)
 print(result)

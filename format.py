b8371828ef886ef348e68b9d77b505c01fac6c94
Andrey42r <168967968+Andrey42r@users.noreply.github.com>
2024-06-26 21:04:03 +0700

    Add files via upload

---
+++ format.py
@@ -0,0 +1,22 @@
+team_num1 = 5
+print('В команде Мастера кода участников: %s ! ' % (team_num1))
+
+team_num2 = 6
+print('Итого сегодня в командах участников: %s и %s !' % (team_num1, team_num2))
+
+score_2 = 42
+print('Команда Волшебники данных решила задач: {} !'.format(score_2))
+
+team1_time = 18015.2
+print('Волшебники данных решили задачи за {} с !'.format(team1_time))
+
+score_1 = 40
+score_2 = 42
+print(f'Команды решили {score_1} и {score_2} задач.')
+
+challenge_result = 'Мастера кода!'
+print(f'Результат битвы: победа команды {challenge_result}!')
+
+tasks_total = 82
+time_avg = 350.4
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.')

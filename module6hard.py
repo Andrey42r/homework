8052581dacb3756cf99697d21219be2f88feac82
Andrey42r <168967968+Andrey42r@users.noreply.github.com>
2024-08-19 13:53:21 +0700

    Add files via upload

--- module6hard.py
+++ module6hard.py
@@ -6,21 +6,20 @@ class Figure:
     sides_count = 0
 
     def __init__(self, color, *sides, filled=True):
-        self.__sides = sides
-        self.__color = color
+        self.__sides = list(sides)
+        self.__color = list(color)
         self.filled = filled
 
     def get_color(self):
         return self.__color
 
-    def _is_valid_color(self, r, g, b):
-        valid_values = 0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255
-        valid_types = isinstance(r, int) and isinstance(g, int) and isinstance(b, int)
-        return valid_types and valid_values
+    def __is_valid_color(self, r, g, b):
+        return all(isinstance(colors1, int) and 0 <= colors1 <= 255 for colors1 in (r, g, b))
 
     def set_color(self, r, g, b):
-        if self.__is_valid_sides(r, g, b):
-            self.__color = r, g, b
+        if self.__is_valid_color(r, g, b):
+            self.__color = (r, g, b)
+
 
     def __is_valid_sides(self, *sides):
         for side in sides:
@@ -34,17 +33,16 @@ class Figure:
     def __len__(self):
         return sum(self.__sides)
 
-    def set_sides(self, *sides):
-        if len(sides) == len(self.__sides):
+    def set_sides(self, *new_sides):
+        if len(new_sides) == len(self.__sides):
             valid_sides = []
-            for side in sides:
+            for side in new_sides:
                 if self.__is_valid_sides(side):
                     valid_sides.append(side)
 
             self.__sides = valid_sides
 
 class Circle(Figure):
-
     sides_count = 1
 
     def __init__(self, color, sides, filled=False):
@@ -65,7 +63,7 @@ class Triangle(Figure):
 
     def get_square(self):
         p = (self.__height / 2)
-        sides = self.get.sides()
+        sides = self.get_sides()
         return math.sqrt((p * (p - sides[0]) * (p - sides[1]) * (p - sides[2])))
 
 

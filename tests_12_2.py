fb636454437db551185bb5e94e64ad97c16dd3e2
Andrey42r <168967968+Andrey42r@users.noreply.github.com>
2024-09-04 19:46:39 +0700

    Add files via upload

--- tests_12_2.py
+++ tests_12_2.py
@@ -1,5 +1,4 @@
 from unittest import TestCase
-import unittest
 
 
 class Runner:
@@ -15,6 +14,9 @@ class Runner:
         self.distance += self.speed
 
     def __str__(self):
+        return self.name
+
+    def __repr__(self):
         return self.name
 
     def __eq__(self, other):
@@ -42,42 +44,35 @@ class Tournament:
 
         return finishers
 
-
 class TournamentTest(TestCase):
     @classmethod
-    def setUpClass(self):
-        self.all_results = {}
+    def setUpClass(cls):
+        cls.all_results = []
 
     def setUp(self):
-        self.usain = Runner('Усэйн', 10)
-        self.andrey = Runner('Андрей', 9)
-        self.nick = Runner('Ник', 3)
+        self.run_1 = Runner('Усейн', 10)
+        self.run_2 = Runner('Андрей', 9)
+        self.run_3 = Runner('Ник', 3)
+
+    def test_run_1_tournament(self):
+        tournament = Tournament(90, self.run_1, self.run_3)
+        results = tournament.start()
+        TournamentTest.all_results.append(results)
+        self.assertTrue(results[2] == 'Ник')
+
+    def test_run_2_tournament(self):
+        tournament = Tournament(90, self.run_2, self.run_3)
+        results = tournament.start()
+        TournamentTest.all_results.append(results)
+        self.assertTrue(results[2] == 'Ник')
+
+    def test_run_3_tournament(self):
+        tournament = Tournament(90, self.run_1, self.run_2, self.run_3)
+        results = tournament.start()
+        TournamentTest.all_results.append(results)
+        self.assertTrue(results[3] == 'Ник')
 
     @classmethod
-    def tearDownClass(self):
-        result = {place: runner.name for place, runner in self.all_results.items()}
-        print(f'{result}')
-
-    def Tournament_test_1(self):
-        tournament = Tournament(90, self.usain, self.nick)
-        results = tournament.start()
-        self.all_results.update(results)
-        self.assertEqual(results[-1], self.nick)
-        self.tearDownClass()
-
-    def Tournament_test_2(self):
-        tournament = Tournament(90, self.andrey, self.nick)
-        results = tournament.start()
-        self.all_results.update(results)
-        self.assertEqual(results[-1], self.nick)
-        self.tearDownClass()
-
-    def Tournament_test_3(self):
-        tournament = Tournament(90, self.usain, self.andrey, self.nick)
-        results = tournament.start()
-        self.all_results.update(results)
-        self.assertEqual(results[-1], self.nick)
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def tearDownClass(cls):
+        for i, elem in enumerate(cls.all_results):
+            print(f'{i + 1}. {elem}')

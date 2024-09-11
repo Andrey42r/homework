32dffbc42283c85ecaacbbba0a1020da5f838210
Andrey42r <168967968+Andrey42r@users.noreply.github.com>
2024-09-11 18:24:25 +0700

    Update module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -13,11 +13,11 @@ dp = Dispatcher()
 
 @dp.message(Command('start'))
 async def cmd_start(message: types.Message):
-    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    print('Привет! Я бот помогающий твоему здоровью.')
 
 @dp.message()
 async def all_messages(message):
-    await message.answer('Введите команду /start, чтобы начать общение.')
+    print('Введите команду /start, чтобы начать общение.')
 
 async def main():
     await dp.start_polling(bot)

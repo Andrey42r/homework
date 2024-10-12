51349f5590da58cbee89d501bef8b51bed900bb7
Andrey42r <168967968+Andrey42r@users.noreply.github.com>
2024-10-12 16:42:03 +0700

    Add files via upload

--- module_16_4.py
+++ module_16_4.py
@@ -17,14 +17,14 @@ def get_all_messages() -> List[User]:
 
 
 @app.post("/user/{username}/{age}")
-def create_message(username: str, age: str):
+def create_message(username: str, age: int):
     user_id = max(users, key=lambda x: int(x.id)).id + 1 if users else 1
     user = User(id=user_id, username=username, age=age)
     users.append(user)
     return users
 
 @app.put("/user/{user_id}/{username}/{age}")
-def create1_message(user_id: str, username: str, age: str) -> str:
+def create1_message(user_id: int, username: str, age: int) -> str:
     try:
         if user_id in users:
             user_id = f"Имя: {username}, возраст: {age}"
@@ -37,5 +37,5 @@ def delete_user(user_id: int) -> str:
     for i, user in enumerate(users):
         if user.id == user_id:
             deleted_user = users.pop(i)
-            return deleted_user
+            return f"User {deleted_user.username} was deleted"
         raise HTTPException(status_code=404, detail="User was not found")
